262b8636b8f7f772338e50a4336e28d0828e24a5
yuanyizheng191-code <yuanyizheng191@gmail.com>
2025-11-29 17:11:02 +0800

    Add files via upload

---
+++ VisitorComparator.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class VisitorComparator implements Comparator<Visitor> {
+    @Override
+    public int compare(Visitor v1, Visitor v2) {
+        // First sort by name
+        int nameComparison = v1.getName().compareTo(v2.getName());
+        if (nameComparison != 0) {
+            return nameComparison;
+        }
+        // If names are the same, sort by age
+        return Integer.compare(v1.getAge(), v2.getAge());
+    }
+}
